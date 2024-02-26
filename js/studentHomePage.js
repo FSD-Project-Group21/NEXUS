e5a9b3768f09e398e5220ca5f8f3c6b4119ff2ad
varshithab05 <varshithab05@gmail.com>
2024-02-26 21:07:24 +0530

    home

--- js/studentHomePage.js
+++ js/studentHomePage.js
@@ -1,13 +1,1 @@
-// sideBar for icon open
-let icons = document.querySelectorAll(".icon-link");
 
-icons.forEach(icon => {
-    icon.addEventListener("click",function(){
-        icon.classList.add("open");
-        for(var i=0;i<icons.length;i++){
-            if(icons[i]!== icon){
-                icons[i].classList.remove("open");
-            }
-        }
-    });
-});
