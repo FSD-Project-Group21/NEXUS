2f525389854b620512c318ac87bfa10df64a9409
SHAGANTI NANDHITHA <nandhithashaganti@gmail.com>
2024-03-16 16:58:25 +0530

    profile page slider

--- public/js/profilePage.js
+++ public/js/profilePage.js
@@ -72,4 +72,36 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+// sliderrr
 
+var btn = document.getElementsByClassName("next-btn");
+var slide = document.getElementById("slide");
+
+btn[0].onclick = function(){
+    slide.style.transform = "translateX(0px)";
+    for(i=0;i<4;i++){
+        btn[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+btn[1].onclick = function(){
+    slide.style.transform = "translateX(-25%)";
+    for(i=0;i<4;i++){
+        btn[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+btn[2].onclick = function(){
+    slide.style.transform = "translateX(-50%)";
+    for(i=0;i<4;i++){
+        btn[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+btn[3].onclick = function(){
+    slide.style.transform = "translateX(-75%)";
+    for(i=0;i<4;i++){
+        btn[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
