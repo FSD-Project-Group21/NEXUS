a5e079eb73d83fe2c0eb6da4e6a4006b8f531831
varshithab05 <varshithab05@gmail.com>
2024-03-17 20:46:47 +0530

    postPageModified

--- public/js/postPage.js
+++ public/js/postPage.js
@@ -20,3 +20,52 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 300);
     });
 });
+const like = document.querySelector('.like-btn');
+const save = document.querySelector('.save-btn');
+const comment = document.querySelector('.comment-btn');
+like.addEventListener('click',()=>{
+    if(!like.classList.contains('liked')){
+        like.innerHTML = '<i class="fa-solid fa-heart" style="color: #ff0000;"></i>';
+        like.classList.add('liked');
+    }else{
+        like.innerHTML = '<i class="fa-regular fa-heart"></i>';
+        like.classList.remove('liked');
+    }
+});
+save.addEventListener('click',()=>{
+    if(!save.classList.contains('saved')){
+        save.innerHTML = '<i class="fa-solid fa-bookmark" style="color: #080d96;"></i>';
+        save.classList.add('saved');
+    }else{
+        save.innerHTML = '<i class="fa-regular fa-bookmark"></i>';
+        save.classList.remove('saved');
+    }
+});
+comment.addEventListener('click',()=>{
+    if(!comment.classList.contains('commented')){
+        comment.innerHTML = '<i class="fa-solid fa-comment"></i>';
+        comment.classList.add('commented');
+    }else{
+        comment.innerHTML = '<i class="fa-regular fa-comment"></i>';
+        comment.classList.remove('commented');
+    }
+});
+const sendButton = document.querySelector('#send-comment');
+const inputComment = document.querySelector('#input-comment');
+const userProfile = document.querySelector('#user-profile');
+const comments = document.querySelector('.comments');
+sendButton.addEventListener('click',()=>{
+    if(inputComment.value){
+        let newElement = document.createElement('div');
+        newElement.className = "comment";
+        let newImage = document.createElement('img');
+        newImage.src = userProfile.src;
+        newElement.appendChild(newImage);
+        let commentText = document.createElement('div');
+        commentText.className = "comment-text";
+        commentText.innerHTML = inputComment.value;
+        inputComment.value = "";
+        newElement.appendChild(commentText);
+        comments.appendChild(newElement);
+    }
+});
