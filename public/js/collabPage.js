1f8b402cd365a2f0d7b7adda8dc3ea0beec2527b
SHAGANTI NANDHITHA <nandhithashaganti@gmail.com>
2024-03-17 18:04:16 +0530

    collabPage

--- public/js/collabPage.js
+++ public/js/collabPage.js
@@ -9,20 +9,34 @@ function toggleButton(button) {
 }
 const collab1 = document.querySelector('._collab1');
 const collab2 = document.querySelector('._collab2');
+const collab0 = document.querySelector('._collab0');
+const collabcont0 = document.querySelector('.collab-container0');
 const collabcont1 = document.querySelector('.collab-container1');
-const collabCont2 = document.querySelector('.collab-container2');
+const collabcont2 = document.querySelector('.collab-container2');
 
+collab0.addEventListener('click',()=>{
+    collab0.classList.add('collab-active');
+    collab1.classList.remove('collab-active');
+    collab2.classList.remove('collab-active');
+    collabcont0.style.display = "flex";
+    collabcont1.style.display = "none";
+    collabcont2.style.display = "none";
+});
 collab1.addEventListener('click',()=>{
+    collab0.classList.remove('collab-active');
     collab1.classList.add('collab-active');
     collab2.classList.remove('collab-active');
     collabcont1.style.display = "flex";
-    collabCont2.style.display = "none";
+    collabcont2.style.display = "none";
+    collabcont0.style.display = "none";
 });
 collab2.addEventListener('click',()=>{
-    collab2.classList.add('collab-active');
+    collab0.classList.remove('collab-active');
     collab1.classList.remove('collab-active');
+    collab2.classList.add('collab-active');
+    collabcont0.style.display = "none";
     collabcont1.style.display = "none";
-    collabCont2.style.display = "flex";
+    collabcont2.style.display = "flex";
 });
 
 
