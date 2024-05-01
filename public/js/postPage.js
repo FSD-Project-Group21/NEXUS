1e448631f5eaee8e7e0c1304b483cd581cb8c603
Ananya5104 <saiananyakatakam@gmail.com>
2024-05-01 10:53:36 +0530

    report

--- public/js/postPage.js
+++ public/js/postPage.js
@@ -20,6 +20,28 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 300);
     });
 });
+document.addEventListener('DOMContentLoaded', function() {
+    const settingsButton = document.querySelector('.report-button');
+    const popupContainer = document.getElementById('report-popup-container');
+    const popup = document.getElementById('report-popup');
+    const closeButton = document.getElementById('close-report-popup');
+
+    settingsButton.addEventListener('click', function() {
+        popupContainer.style.display = 'flex';
+        setTimeout(function() {
+            popup.style.transform = 'scale(1)';
+            popup.style.opacity = '1';
+        }, 100);
+    });
+
+    closeButton.addEventListener('click', function() {
+        popup.style.transform = 'scale(0.5)';
+        popup.style.opacity = '0';
+        setTimeout(function() {
+            popupContainer.style.display = 'none';
+        }, 300);
+    });
+});
 const like = document.querySelector('.like-btn');
 const save = document.querySelector('.save-btn');
 const comment = document.querySelector('.comment-btn');
