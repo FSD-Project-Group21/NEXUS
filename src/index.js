61ef23de5f014ea2277275268fb475a9facce5a4
Ananya5104 <saiananyakatakam@gmail.com>
2024-05-01 15:22:33 +0530

    index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
 const bcryptjs = require("bcryptjs")
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
